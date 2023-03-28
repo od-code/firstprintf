43ccbd6814808407cebc37c6ea62c47c5b7d54bf
Derrick Atongo <derrickatongo@gmail.com>
2023-03-27 17:23:39 -0700

    Added header file

--- main.h
+++ main.h
@@ -1,9 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef PRINT_F
+#define PRINT_F
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-
-
-
 
 #endif
